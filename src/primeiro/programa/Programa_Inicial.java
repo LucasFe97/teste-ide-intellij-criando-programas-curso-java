3df4f2c6a72a17581f966f6e11b0c483808399e5
LucasFe97 <lucasferreirasd82@gmail.com>
2023-04-17 22:49:33 -0300

    first commit

---
+++ src/primeiro/programa/Programa_Inicial.java
@@ -0,0 +1,33 @@
+package primeiro.programa;
+
+import primeiro.programa.model.Gato;
+
+public class Programa_Inicial {
+    public static void main(String[] args ) {
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+/*        int a = 2;
+        int b = 5;
+
+        System.out.println("Hello world! " + (a + b));*/
+
+    }
+
+
+}
+
+
+
+
+
+/*    public static void main(String[] args) {
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+        int a = 2;
+        int b = 5;
+        System.out.println("Hello world! " + (a + b ));
+    }
+}*/
---
+++ src/primeiro/programa/calculadora/Calculadora.java
@@ -0,0 +1,43 @@
+package primeiro.programa.calculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        double a = 0;
+        double b = 0;
+        System.out.println("Digite o primeiro valor: ");
+        a = scan.nextInt();
+        System.out.println("Digite o segundo valor: ");
+        b = scan.nextInt();
+
+        double soma = soma(a, b);
+        double multiplicacao = multiplicacao(a, b);
+        double subtracao = subtracao(a, b);
+        double divisao = divisao(a, b);
+
+        System.out.println("somar: " + soma);
+        System.out.println("mult: " + multiplicacao);
+        System.out.println("sub: " + subtracao);
+        System.out.println("div: " + divisao );
+    }
+
+        public static double soma (double a, double b) {
+            return a + b;
+        }
+
+        public static double multiplicacao (double a, double b) {
+            return a * b;
+        }
+
+        public static double subtracao (double a, double b) {
+            return a - b;
+        }
+
+        public static double divisao (double a, double b) {
+            return a / b;
+        }
+
+    }
